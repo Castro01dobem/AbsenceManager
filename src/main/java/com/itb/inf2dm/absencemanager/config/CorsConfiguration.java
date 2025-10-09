22b30f2dea9c0ace2319d03729b6d03870c97369
Castro01dobem <rm95036@estudante.fieb.edu.br>
2025-10-09 11:08:46 -0300

    Terceiro commit para salvar

--- src/main/java/com/itb/inf2dm/absencemanager/config/CorsConfiguration.java
+++ src/main/java/com/itb/inf2dm/absencemanager/config/CorsConfiguration.java
@@ -1,4 +1,16 @@
 package com.itb.inf2dm.absencemanager.config;
 
-public class CorsConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8686")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE", "CONNECT");
+    }
 }
