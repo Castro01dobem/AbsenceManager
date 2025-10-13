05651ca9b9920a78eea88d05abaa7c574e238f4a
Castro01dobem <rm95036@estudante.fieb.edu.br>
2025-10-13 12:33:56 -0300

    Commit BelleLigação

--- src/main/java/com/itb/inf2dm/absencemanager/model/repository/AlunoRepository.java
+++ src/main/java/com/itb/inf2dm/absencemanager/model/repository/AlunoRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno,Integer> {
-    Optional<Object> findById();
+
 }
--- src/main/java/com/itb/inf2dm/absencemanager/model/repository/AlunoTurmaRepository.java
+++ src/main/java/com/itb/inf2dm/absencemanager/model/repository/AlunoTurmaRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AlunoTurmaRepository extends JpaRepository<AlunoTurma, Integer> {
-    Optional<Object> findById();
+
 }
